c74338f2073db9b715fc9f000f9edc795515fc54
igarousi <irene.garousi.nejad@gmail.com>
2023-09-28 21:31:41 -0400

    updated Dockerfile and test.sh to execute the entire process.

--- containers/nwm-v2/test.sh
+++ containers/nwm-v2/test.sh
@@ -14,16 +14,16 @@ fi
 IMAGE="igarousi/nwm-subset-test:v2.0"
 
 # Run the docker container
-#docker run --rm -ti \
-#	-v /home/igarousi/nwm-data/nwm.v2.1:/srv/domain \
-#	-v $OUTPUT:/srv/output \
-#        $IMAGE 382582.18746 1720355.72762 367584.87840 1734488.45260 /srv/domain /srv/output
+docker run --rm -ti \
+	-v /home/igarousi/nwm-data/nwm.v2.0.0:/srv/domain \
+	-v $OUTPUT:/srv/output \
+        $IMAGE 382582.18746 1720355.72762 367584.87840 1734488.45260 /srv/domain /srv/output
 
 # Interactive mode test. When activated:
 # Make sure the above docker run command is commented.
 # Also, the last line of the Docker image (ENTRYPOINT ...) should be commented.
-docker run --rm -ti \
-        -v /home/igarousi/nwm-data/nwm.v2.0.0:/srv/domain \
-        -v $OUTPUT:/srv/output \
-	--entrypoint=/bin/bash \
-        $IMAGE 
+#docker run --rm -ti \
+#        -v /home/igarousi/nwm-data/nwm.v2.0.0:/srv/domain \
+#        -v $OUTPUT:/srv/output \
+#	--entrypoint=/bin/bash \
+#        $IMAGE 
