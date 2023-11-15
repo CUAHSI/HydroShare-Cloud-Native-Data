0b4dff9ef101797e15519e2db430b2215c641c64
Anthony Castronova <castronova.anthony@gmail.com>
2023-11-15 15:21:07 -0500

    adding missing python examples

--- containers/forcing/aorc-v1/run.sh
+++ containers/forcing/aorc-v1/run.sh
@@ -4,9 +4,12 @@
 docker run --rm \
     -v $(pwd)/output:/srv/output \
     -v $(pwd)/../../../notebooks/sample-data:/srv/shp-data \
-    cuahsi/aorc:1.1 \
+    cuahsi/aorc:latest \
     "2010-01-01 00:00:00" \
-    "2010-01-01 01:00:00" \
+    "2010-01-01 09:00:00" \
     /srv/shp-data/watershed.shp \
     /srv/output \
+    --timesteps-per-group=3 \
+    --num-workers=3 \
+    --worker-mem-limit=3GB \
     --verbose
--- containers/forcing/aorc-v1/test-entry.sh
+++ containers/forcing/aorc-v1/test-entry.sh
@@ -2,8 +2,9 @@
 
 python entry.py \
     "2010-01-01 00:00:00" \
-    "2010-01-01 01:00:00" \
+    "2010-01-01 09:00:00" \
     $(pwd)/../../../notebooks/sample-data/watershed.shp \
     $(pwd)/output \
+    --timesteps-per-group=3 \
     --verbose
 
