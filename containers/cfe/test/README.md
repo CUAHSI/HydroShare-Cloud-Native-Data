0d46046ad0316f14df80e57002f2c69f3ea85eb4
Anthony Castronova <castronova.anthony@gmail.com>
2024-05-21 14:57:17 -0400

    Added readme to test dir. Updated paths in test-cfe-configure-local.sh

---
+++ containers/cfe/test/README.md
@@ -0,0 +1,21 @@
+# CFE Configuration
+
+
+## Installation
+
+
+Install Python requirements:
+```bash
+pip install -r docker/requirements.txt
+```
+
+Install `ngen-cal` package:
+```bash
+pip install deps/ngen-cal/python/ngen_conf
+```
+
+Install `t-route` package:
+```bash
+pip install deps/t-route/src/troute-config
+```
+
--- containers/cfe/test/test-cfe-configure-local.sh
+++ containers/cfe/test/test-cfe-configure-local.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 root_path="test-data/small"
-python ../entry.py \
+out_path="test-data/small-out"
+python ../src/entry.py \
 	$root_path/domain/wb-2853612_upstream_subset.gpkg \
 	$root_path/forcing/forcing.nc \
 	$root_path/domain/cfe_noahowp_attributes.csv \
-	$root_path/config \
+	$out_path \
 	--verbose
