f1d4d186ae96a28150481d071f02140dfa158561
Anthony Castronova <castronova.anthony@gmail.com>
2024-05-21 09:04:35 -0400

    consolidating scripts and test dirs

--- containers/cfe/scripts/test-cfe-configuration.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python entry.py \
-	test-data/small/domain/wb-2917533_upstream_subset.gpkg \
-	test-data/small/forcing/results.nc \
-	test-data/small/domain/cfe_noahowp_attributes.csv \
-	test-data/small-out \
-	--verbose
--- containers/cfe/test-cfe-configure-local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-python entry.py \
-    input-data/wb-2917533_upstream_subset.gpkg \
-    input-data/results.nc \
-    input-data/cfe_noahowp_attributes.csv \
-    output-data \
-    --verbose
